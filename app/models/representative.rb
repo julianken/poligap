7d80997027edfa617c9b04d15dea483efb248a69
julianken <julian.kennon.d@gmail.com>
2016-11-02 00:24:32 -0700

    party letter computed property added

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,3 +1,9 @@
 class Representative < ApplicationRecord
   belongs_to :state
+
+  attr_reader :party_letter
+
+  def party_letter
+    party[0,1]
+  end
 end
--- app/serializers/representative_serializer.rb
+++ app/serializers/representative_serializer.rb
@@ -1,4 +1,4 @@
 class RepresentativeSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :state_id, :party, :cid, :image_url, :gender,
-            :congress_office
+            :congress_office, :image_url, :party_letter
 end

8d5790d062234ee2501d9bc99ea77b758e8a2930
julianken <julian.kennon.d@gmail.com>
2016-12-06 10:12:11 -0700

    updated representative_serializer to also contain the new properties

--- app/serializers/representative_serializer.rb
+++ app/serializers/representative_serializer.rb
@@ -1,5 +1,7 @@
 class RepresentativeSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :state_id, :party, :cid, :state,
             :image_url, :gender, :congress_office, :image_url, :party_letter,
-            :cash_spent, :cash_on_hand, :cash_debt, :cash_total, :contribution_information
+            :twitter_id, :phone_number, :youtube_url, :facebook_id, :website,
+            :cash_spent, :cash_on_hand, :cash_debt, :cash_total,
+            :contribution_information
 end
